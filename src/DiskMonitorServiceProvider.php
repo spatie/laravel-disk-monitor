647741110758b411225479fc6cac14ed9e9f3b14
Imran Bajerai <i.bajrai@gmail.com>
2021-02-01 06:57:03 +0800

    Check that the correct class when publishing migrations

--- src/DiskMonitorServiceProvider.php
+++ src/DiskMonitorServiceProvider.php
@@ -36,7 +36,7 @@ class DiskMonitorServiceProvider extends ServiceProvider
             __DIR__ . '/../resources/views' => base_path('resources/views/vendor/disk-monitor'),
         ], 'views');
 
-        if (! class_exists('CreatePackageTables')) {
+        if (! class_exists('CreateDiskMonitorTables')) {
             $this->publishes([
                 __DIR__ . '/../database/migrations/create_disk_monitor_tables.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_disk_monitor_tables.php'),
             ], 'migrations');
