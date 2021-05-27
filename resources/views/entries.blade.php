0d18a89ea8c220540e150f504e178ad93628ec92
Patompong Savaengsuk <p.pangpond@gmail.com>
2021-05-27 19:52:39 +0700

    Add disk size column to the view

--- resources/views/entries.blade.php
+++ resources/views/entries.blade.php
@@ -5,6 +5,7 @@
         <tr>
             <th>Disk name</th>
             <th>File count</th>
+            <th>Disk size (byte)</th>
             <th>Recorded at</th>
         </tr>
     </thead>
@@ -13,6 +14,7 @@
             <tr>
                 <td>{{ $entry->disk_name }}</td>
                 <td>{{ $entry->file_count }}</td>
+                <td>{{ number_format($entry->disk_size) }}</td>
                 <td>{{ $entry->created_at->format('Y-m-d H:i:s') }}</td>
             </tr>
         @endforeach
